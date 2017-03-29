b592bdf8b5b5df07b76a65d096a3559ff81351b2
Alexander Hultnér <ahultner@gmail.com>
2017-03-29 15:57:42 +0200

    Added some more paths

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/trailing-route/')
+def trailer():
+    return 'A trailing route redirects non trailing uris'
+
+
+@app.route('absolute')
+def absoluter():
+    return 'An absolute route will return 404 if trailing slash is added to uri'
+
