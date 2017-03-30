946986323a50a8e64afd297ea6992c7df8aaab1e
Alexander Hultnér <ahultner@gmail.com>
2017-03-30 12:08:08 +0200

    Aded some more routes with variables

--- app.py
+++ app.py
@@ -2,6 +2,22 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return 'Index Page'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+   # show the user profile for that user
+   return 'User %s' % username
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/hello')
 def hello_world():
     return 'Hello, World!'
 
