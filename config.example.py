e319f69dafb189322efd1cd231f144e764a4edce
Alexander Hultnér <ahultner@gmail.com>
2017-04-03 17:22:22 +0200

    Added example config to base config.py upon

---
+++ config.example.py
@@ -0,0 +1,10 @@
+# Sample config file, change values and rename to config.py
+# do not push your config upstream
+
+# Generate a good secret using the following code
+# import os
+# os.urandom(24)
+# can also be invoked from commandline by the following code
+# python3 -c "import os; print(os.urandom(24))"
+session_secret = 'CHANGE ME'
+
