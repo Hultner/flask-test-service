fa1da3a58c4469d2baeccf9b363f59e739422294
Alexander Hultnér <ahultner@gmail.com>
2017-04-04 12:29:05 +0200

    Created script for executing linting on all code

---
+++ lint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+## DESCRIPTION: Check Python code for PEP 8 coding style adherence 
+## AUTHOR: Alexander Hultnér <alexander@cetrez.com>
+## USAGE: ./lint.sh
+
+# Find all .py files which are not from virtualenv and run pycodestyle on them
+find . -iname "*.py" \
+    -not -path "./flask-test-service/*" \
+    -exec pycodestyle {} \;
