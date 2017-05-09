8cc6dcb6536d3ca00e52233a20b9aa48e1cc0213
Alexander Hultnér <ahultner@gmail.com>
2017-05-09 10:37:31 +0200

    Updated example config to include all new parameters available

--- test_service/config.example.py
+++ test_service/config.example.py
@@ -4,15 +4,28 @@ This module store configurable python parameters set by the end user.
 Rename rename your configuration file to config.py and do not push your changes
 upstream.
 
-Attributes:
-    SESSION_SECRET (str): Key used to encrypt sessions
-"""
-
-
-SESSION_SECRET = 'CHANGE ME'
-""" Generate a good secret using the following code
+Generate a good secret using the following code
 import os
 os.urandom(24)
-can also be invoked from commandline by the following code
+This can also be invoked from commandline by the following code
 python3 -c "import os; print(os.urandom(24))"
+
+Attributes:
+    SESSION_SECRET (str): Key used to encrypt sessions
+    DB_USER (str): PostgreSQL user
+    DB_PASSWORD (str): PostgreSQL password, empty if peer auth
+    DB_DATABASE (str): Database to work against
+    DB_SCHEMA (str): Not sure if this should be configurable, target schema
+    DB_HOST (str): Database server, default is localhost
+    DB_PORT (int): Port the database server is running on
 """
+
+SESSION_SECRET = 'CHANGE ME'
+
+"""Database connection configuration """
+DB_USER = 'postgres'
+DB_PASSWORD = ''
+DB_DATABASE = 'experiment'
+DB_SCHEMA = 'notes'
+DB_HOST = 'localhost'
+DB_PORT = 5432
